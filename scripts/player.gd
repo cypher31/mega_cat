57e73c52675e1e1078c0fff6c03ad30490c31d16
cypher31 <kelbystyler@gmail.com>
2018-02-04 13:21:14 -0800

    Stop movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var player_speed = Vector2(100, 100)
+var player_speed = Vector2(1000, 1000)
 var player_aim
 var player_direction_y = 0
 var player_direction_x = 0
@@ -17,7 +17,10 @@ func _process(delta):
 		player_move = true
 	elif Input.is_action_pressed("direction_down"):
 		player_direction_y = 1
-		player_move = true
+		player_move = true 
+	else:
+		player_move = false
+		pass
 	
 	if Input.is_action_pressed("direction_left"):
 		player_direction_x = -1
@@ -25,6 +28,8 @@ func _process(delta):
 	elif Input.is_action_pressed("direction_right"):
 		player_direction_x = 1
 		player_move = true
+	else:
+		player_move = false
 		pass
 	
 	
@@ -33,5 +38,6 @@ func _process(delta):
 		var velocity_y = player_direction_y * player_speed.y * delta
 		
 		move_and_slide(Vector2(velocity_x, velocity_y))
+		print(global_position)
 		pass
 	pass
