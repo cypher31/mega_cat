f8ae3e8c56eda15b4062f293ea43c15a48c44d1e
cypher31 <kelbystyler@gmail.com>
2018-02-04 13:42:17 -0800

    Player speed and look at

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var player_speed = Vector2(1000, 1000)
+var player_speed = 10000
 var player_aim
 var player_direction_y = 0
 var player_direction_x = 0
@@ -12,6 +12,9 @@ func _ready():
 	pass
 
 func _process(delta):
+	look_at(get_global_mouse_position())
+	
+	#upwards movement
 	if Input.is_action_pressed("direction_up"):
 		player_direction_y = -1
 		player_move = true
@@ -22,6 +25,7 @@ func _process(delta):
 		player_move = false
 		pass
 	
+	#downwards movement
 	if Input.is_action_pressed("direction_left"):
 		player_direction_x = -1
 		player_move = true
@@ -34,8 +38,8 @@ func _process(delta):
 	
 	
 	if player_move:
-		var velocity_x = player_direction_x * player_speed.x * delta
-		var velocity_y = player_direction_y * player_speed.y * delta
+		var velocity_x = player_direction_x * player_speed * delta
+		var velocity_y = player_direction_y * player_speed * delta
 		
 		move_and_slide(Vector2(velocity_x, velocity_y))
 		print(global_position)
